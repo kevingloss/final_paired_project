24535c7c2b1a54c57a42e8345e719ca0701701ec
Kevin Gloss <kgloss89@gmail.com>
2021-11-09 19:24:35 -0700

    Feat driver? and drive methods

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -4,9 +4,18 @@ class Passenger
   def initialize(attributes)
     @name = attributes['name']
     @age = attributes['age']
+    @driver = false
   end
 
   def adult?
     @age >= 18
+  end
+
+  def driver?
+    @driver
+  end
+
+  def drive
+    @driver = true if adult? == true
   end
 end
