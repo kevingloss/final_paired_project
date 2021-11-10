450df392b8835121629ff8ce274de191ea455cd8
Kevin Gloss <kgloss89@gmail.com>
2021-11-09 19:38:15 -0700

    Test add passenger method

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe Vehicle do
   describe 'Vehicle Class' do
     before(:each) do
       @vehicle = Vehicle.new("2001", "Honda", "Civic")
+      @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      @jude = Passenger.new({"name" => "Jude", "age" => 20})
+      @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
     end
 
     it 'exists' do
@@ -25,6 +28,16 @@ RSpec.describe Vehicle do
       @vehicle.speed
 
       expect(@vehicle.speeding?).to be(true)
+    end
+
+    it 'can add passengers' do
+      expect(@vehicle.passengers).to eq([])
+
+      @vehicle.add_passenger(@charlie)
+      @vehicle.add_passenger(@jude)
+      @vehicle.add_passenger(@taylor)
+
+      expect(@vehicle.passengers).to eq([@charlie, @jude, @taylor])
     end
   end
 end
