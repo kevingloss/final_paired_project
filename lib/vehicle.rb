873bc634e5109188d933734841940f701ab7ed30
Kevin Gloss <kgloss89@gmail.com>
2021-11-09 19:35:02 -0700

    Feat speeding/speed methods

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,9 +1,19 @@
 class Vehicle
-  attr_reader :year, :make, :model
+  attr_reader :year, :make, :model, :passengers
 
   def initialize(year, make, model)
     @year = year
     @make = make
     @model = model
+    @speeding = false
+    @passengers = []
+  end
+
+  def speeding?
+    @speeding
+  end
+
+  def speed
+    @speeding = true
   end
 end
